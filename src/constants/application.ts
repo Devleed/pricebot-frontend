794a282574a74b28749f3b261a1d19289dfb8130
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-11-10 15:29:33 +0500

    changes: server URL updated

--- src/constants/application.ts
+++ src/constants/application.ts
@@ -1,4 +1,4 @@
-export const APPLICATION_URL = 'http://localhost:3000'
+export const APPLICATION_URL = 'http://44.212.190.75/'
 export const APPLICATION_NAME = 'xord app'
 
-export const SERVER_URL = 'http://localhost:4999/'
+export const SERVER_URL = 'http://44.212.190.75:4999/'
